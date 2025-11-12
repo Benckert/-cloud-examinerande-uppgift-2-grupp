530d09b6b52a890e64c02d942cf74ade552a4348
stalstierna <andrea.stalstierna@chasacademy.se>
2025-11-12 09:23:28 +0100

    lint-errors

--- backend/src/routes/ai.routes.ts
+++ backend/src/routes/ai.routes.ts
@@ -1,9 +1,8 @@
 import { aiSummary } from "../controllers/ai.controllers.js";
 import { Router } from "express";
-import { auth } from "../middleware/auth.middleware.js"
+import { auth } from "../middleware/auth.middleware.js";
 
 const router = Router();
-
 
 router.post("/ai-summary", auth, aiSummary);
 
